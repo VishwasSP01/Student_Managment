d2c24dae6c6ef1bf27ff15dc30b2d691001c36d7
VishwasSP01 <vishwasvishu555@gmail.com>
2025-04-08 20:28:10 +0530

    Used HashMap as database insted of actual DB

---
+++ Student-Managment/src/main/java/com/HakunaMtata/Student/Managment/StudentManagmentApplication.java
@@ -0,0 +1,14 @@
+package com.HakunaMtata.Student.Managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagmentApplication.class, args);
+	}
+
+}
+
