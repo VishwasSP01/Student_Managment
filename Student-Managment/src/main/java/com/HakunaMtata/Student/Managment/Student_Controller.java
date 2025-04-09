f290655c838be5018798471b8dc8135b0305564d
VishwasSP01 <vishwasvishu555@gmail.com>
2025-04-09 20:19:55 +0530

    Passed RequestEntity

--- Student-Managment/src/main/java/com/HakunaMtata/Student/Managment/Student_Controller.java
+++ Student-Managment/src/main/java/com/HakunaMtata/Student/Managment/Student_Controller.java
@@ -14,34 +14,40 @@ public class Student_Controller {
     @Autowired
     StudentService studentService; // Object of StudentService class is created
     
-
+    // getting student information by admission number
     @GetMapping("/getStudent/{admissionNo}")
-    public  Student getStudent(@PathVariable int admissionNo) {
-        return studentService.getStudent(admissionNo);
+    public  ResponseEntity getStudent(@PathVariable int admissionNo) {
+        Student student = studentService.getStudent(admissionNo);
+        return new ResponseEntity<>(student, HttpStatus.FOUND); // Returning the student object with HTTP status OK
     }
 
     // adding student information
     @PostMapping("/addStudent")
     public String addStudent(@RequestBody Student student) {
-        return studentService.addStudent(student);
+        String response = studentService.addStudent(student);;
+        return new ResponseEntity<>(response, HttpStatus.CREATED).toString(); // Returning the response with HTTP status CREATED
+
     }
 
     // updating student information (age)
     @PutMapping("/updateStudent")
     public String updateStudent(@RequestParam int admissionNo, @RequestParam int age) {
-        return studentService.updateStudent(admissionNo, age);
+        String response = studentService.updateStudent(admissionNo, age);
+        return new ResponseEntity<>(response, HttpStatus.OK).toString(); // Returning the response with HTTP status OK
     }
 
     // deleting student information
     @DeleteMapping("/deleteStudent/{admissionNo}")
     public String deleteStudent(@PathVariable int admissionNo) {
-        return studentService.deleteStudent(admissionNo);
+        String response = studentService.deleteStudent(admissionNo);
+        return new ResponseEntity<>(response, HttpStatus.GONE).toString(); // Returning the response with HTTP status OK
     }
 
     // getting all student information
     @GetMapping("/getAllStudents")
     public Map<Integer, Student> getAllStudents() {
-        return studentService.getAllStudents();
+        Map<Integer, Student> studends = studentService.getAllStudents();
+        return new ResponseEntity<>(studends, HttpStatus.FOUND).getBody(); // Returning the map of students with HTTP status OK
     }
 
 
